a0993ed02e88ebe76924f3a8ad23d13b598d1b72
predrag milosavljevic <predragmilosavljevic88@gmail.com>
2016-05-12 16:42:43 +0200

    added separate js files, json objects, js inheritance

--- public/app.js
+++ public/app.js
@@ -1,8 +1,54 @@
 
-var testVar = 'iAmGlobalVar';
+var testVar = 'globalVarInsideAppJS';
 
 var helloWorld = function () {
     alert('Hello world');
 
-    var testVar = 'iAmVarInsideAnonymusFunction';
-}
+    var testVar = 'localVarInsideAnonymusFunction';
+};
+
+
+
+var jsonObject ={"first_name":"John","last_name":" Johnson","company":"vivifyIdeas"};
+
+var jsonArray = {"employees":[
+    {"first_name":"John", "last_name":"Doe"},
+    {"first_name":"Anna",	"last_name":"Smith"},
+    {"first_name":"Peter", "last_name":"Jones"}
+    ]
+};
+
+
+function Person(fname, lname, gender) {
+    this.first_name = fname;
+    this.last_name = lname;
+    this.gender = gender;
+};
+
+Person.prototype.getInfo = function() {
+    return 'First Name: '+ this.first_name + ' Last Name: ' + this.last_name + ' Gender: ' + this.gender;
+};
+Person.prototype.sayHello = function () {
+    console.log("Hello, I'm " + this.first_name);
+};
+
+    var youngBoy = new Person('Peter', 'Pan', 'male');
+
+
+function Girl(age) {
+    Person.call(this);
+    this.age = age;
+};
+
+Girl.prototype = Object.create(Person.prototype);
+
+    var youngGirl = new Girl(8);
+
+
+//Static property
+Person.height = 150;
+
+//Static function
+Person.drink = function (drinkName) {
+    console.log('I am drinking ' + drinkName);
+};
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,6 +24,10 @@
             margin-right: 6px;
         }
     </style>
+
+    <script>
+        var testVar = 'varInsideScriptTagOnPage';
+    </script>
 </head>
 <body id="app-layout">
 
@@ -37,5 +41,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+    <script src="anotherJsTestFile.js"></script>
+    <script src="app.js"></script>
+
 </body>
 </html>
